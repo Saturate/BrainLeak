4aa1bed1c286b8d70bcc07df46430f4660c1453c
Kristian Brink Gansted <kbri@itu.dk>
2016-04-09 17:12:06 +0200

    added rising water graphics and water/footstep sounds

---
+++ unity/Assets/Scripts/footsteps.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class footsteps : MonoBehaviour {
+
+  private CharacterController cc;
+  private AudioSource audio;
+	// Use this for initialization
+	void Start () {
+    cc = GetComponent<CharacterController>();
+    audio = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	  if(cc.velocity.magnitude > 0 && !audio.isPlaying) {
+      audio.Play();
+    }
+	}
+}
---
+++ unity/Assets/Scripts/waterRising.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class waterRising : MonoBehaviour {
+
+  public float RiseSpeed;
+	
+	// Update is called once per frame
+	void Update () {
+    transform.position += Vector3.up * RiseSpeed;
+  }
+}
