ffc678b580f3b72ee0d7e8f6f70f278d725fd05e
galtrold <james@galtrold.dk>
2016-04-09 15:55:54 +0200

    Initial unity commit.

---
+++ unity/Assets/Scripts/CharController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharController : MonoBehaviour {
+
+    CharacterController cc;
+    public float speed;    
+    // Use this for initialization
+    void Start () {
+
+        cc = GetComponent<CharacterController>();
+	}
+	
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        transform.Rotate(new Vector3(0.0f, moveHorizontal * speed*10));
+        var test = transform.TransformDirection(0.0f, 0.0f, moveVertical * speed);
+        Vector3 movement = new Vector3(0.0f, 0.0f, moveVertical* speed);
+        
+        cc.Move(test);
+        
+
+       
+    }
+}
