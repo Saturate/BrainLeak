91e8d553cdcfcf1781447feaeb5ba7943643f037
Kristian Brink Gansted <kbri@itu.dk>
2016-04-09 23:57:18 +0200

    added end state and drowning

--- unity/Assets/Scripts/CharController.cs
+++ unity/Assets/Scripts/CharController.cs
@@ -3,25 +3,29 @@ using System.Collections;
 
 public class CharController : MonoBehaviour {
 
-    CharacterController cc;
-    public float speed;    
-    // Use this for initialization
-    void Start () {
+  public GUIText WinText;
+  public GUIText LossText;
+  CharacterController cc;
 
-        cc = GetComponent<CharacterController>();
-	}
-	
-    void Update()
-    {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-        transform.Rotate(new Vector3(0.0f, moveHorizontal * speed*10));
-        var test = transform.TransformDirection(0.0f, 0.0f, moveVertical * speed);
-        Vector3 movement = new Vector3(0.0f, 0.0f, moveVertical* speed);
-        
-        cc.Move(test);
-        
+  public float speed;
+  // Use this for initialization
+  void Start() {
+    cc = GetComponent<CharacterController>();
+  }
 
-       
+  void Update() {
+    float moveHorizontal = Input.GetAxis("Horizontal");
+    float moveVertical = Input.GetAxis("Vertical");
+    transform.Rotate(new Vector3(0.0f, moveHorizontal * speed * 10));
+    var test = transform.TransformDirection(0.0f, 0.0f, moveVertical * speed);
+    Vector3 movement = new Vector3(0.0f, 0.0f, moveVertical * speed);
+
+    cc.Move(test);
+  }
+
+  void OnTriggerEnter(Collider collision) {
+    if (collision.gameObject.name == "goal") {
+      Application.LoadLevel("EndGame");
     }
+  }
 }
---
+++ unity/Assets/Scripts/drowning.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Diagnostics;
+
+public class drowning : MonoBehaviour {
+
+  public float MaxWaterLevel;
+  public float SecondUntilDrowning;
+
+  private bool isCountingDown;
+  private Stopwatch stopWatch;
+		
+  void Start() {
+    isCountingDown = false;
+  }
+	// Update is called once per frame
+	void Update () {
+    UnityEngine.Debug.Log(transform.position.y);
+    if (transform.position.y < MaxWaterLevel)
+      return;
+
+    if (!isCountingDown) {
+      stopWatch = Stopwatch.StartNew();
+      isCountingDown = true;
+    } else if (stopWatch.ElapsedMilliseconds > SecondUntilDrowning * 1000)
+      Application.LoadLevel("EndGame");
+
+    if(stopWatch != null)
+      UnityEngine.Debug.Log(stopWatch.ElapsedMilliseconds - SecondUntilDrowning * 1000);
+  }
+}
